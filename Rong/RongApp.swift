143bfc17eedc5aaf6bd1608eb74d6064a6df6a31
Logan Wang <wls0202@gmail.com>
2026-02-22 17:48:32 -0800

    first commit

---
+++ Rong/RongApp.swift
@@ -0,0 +1,17 @@
+//
+//  RongApp.swift
+//  Rong
+//
+//  Created by Logan Wang on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct RongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
